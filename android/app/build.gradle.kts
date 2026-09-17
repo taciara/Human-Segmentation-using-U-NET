010ff5dc14392603a4cc3b7cab1da234c7bfab50
Taciara Furtado <contato@taciara.com.br>
2026-09-17 16:02:27 -0300

    nova versao

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "online.seuprojeto.filtrofanta"
         minSdk = 24
         targetSdk = 35
-        versionCode = 56
-        versionName = "5.4.8-native"
+        versionCode = 58
+        versionName = "5.4.10-native"
         buildConfigField("String", "SHARE_ORIGIN", "\"https://fanta-filtro.seuprojeto.online\"")
     }
     androidResources {
